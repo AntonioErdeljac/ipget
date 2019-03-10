2667664e0bfcfe342216a17c2608d20488b9d4e4
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2019-03-10 20:41:36 +0100

    x

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.get('/', (req, res) => {
   console.log((req.headers['x-forwarded-for'] || '').split(',').pop())
-  console.log(req.connection.remoteAddress)
-  console.log(req.socket.remoteAddress)
-  console.log(req.connection.socket.remoteAddress)
+  console.log(req.connection && req.connection.remoteAddress)
+  console.log(req.socket && req.socket.remoteAddress)
+  console.log(req.connection && req.connection.socket && req.connection.socket.remoteAddress)
 });
 
 http.createServer(app).listen(process.env.PORT || 3001);
