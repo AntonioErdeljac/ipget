0c56dfc67ffb409a2d39d6a4a33df6a59e0db260
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2019-03-10 20:37:17 +0100

    updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(req.connection.remoteAddress);
+  console.log(req.headers['X-Forwarded-For']);
 });
 
 http.createServer(app).listen(process.env.PORT || 3001);
